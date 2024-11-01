d8c377dd49bc5d8b96625316b035ced472b884fa
Gustavo-poi <00001114736971SP@al.educacao.sp.gov.br>
2024-11-01 18:29:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const avanca = document.querySelectorAll('.btn-peoximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function () {
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-0' + this.getAttribute('data-proximo');
-        atual.classList.remove('ativo')
-        document.getElementById(proximoPasso).classList.add('ativo')
-    })
-})
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    });
+});
